1b6fd59e4d231eb85df525dfa6370bd8addccc0d
Joshua Clement <jaclem12@gmail.com>
2022-01-03 20:40:34 -0500

    added js to allow for clicking and moving mouse

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
+const divs = document.querySelectorAll('div');
+
+
+
 const squareCreate = function(){
 
     let divContainer = document.createElement('container');
@@ -8,8 +12,47 @@ const squareCreate = function(){
     // loops through and creates / appends 256 child divs
     for(let i = 1; i <= 256; i++){
         let childDivs = document.createElement('div');
+
+        childDivs.setAttribute('class', 'squares');
         divContainer.appendChild(childDivs);
     }
 }
 
+const squareColor = function(){
+    let squares = document.querySelectorAll('.squares');
+    let isDrawing = false;
+    let x = 0;
+    let y = 0;
+
+    squares.forEach(item => {
+        item.addEventListener('mousedown', (e)=> {
+            x = e.offsetX;
+            y = e.offsetY;
+            
+            if(x > 0 && y > 0){
+                item.style.backgroundColor = "lightgray";
+                isDrawing = true;
+            }            
+        });
+
+        item.addEventListener('mousemove', () => {
+            if (isDrawing === true) {
+                x = 0;
+                y = 0;
+                
+                item.style.backgroundColor = "lightgray";               
+            }
+        });
+
+        item.addEventListener('mouseup', () => {
+            x = 0;
+            y = 0;
+            isDrawing = false;
+        });
+
+    });
+}
+
 squareCreate();
+squareColor();
+
